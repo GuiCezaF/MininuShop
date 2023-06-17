9305e6dd1314e2ce3d550fcf1074be325d6d1ef7
Guilherme Cezarino Felipe <contato.guicezafe@gmail.com>
2023-06-17 16:24:17 -0300

    chore: add DbContext

---
+++ MininuShop.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using MininuShop.Domain.Entities;
+
+namespace MininuShop.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options) { }
+
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
