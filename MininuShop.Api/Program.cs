0fc4c21895770250467f8f293f1e3fa9910dffb6
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-06-22 13:33:27 -0300

    chore: add Migration and person map

--- MininuShop.Api/Program.cs
+++ MininuShop.Api/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MininuShop.Infra.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly("MininuShop.Api")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ MininuShop.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MininuShop.Domain.Entities;
+
+namespace MininuShop.Infra.Data.Maps
+{
+    public class PersonMap : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.HasMany(c => c.Purchases).WithOne(p => p.Person).HasForeignKey(c => c.PersonId);
+        }
+    }
+}
