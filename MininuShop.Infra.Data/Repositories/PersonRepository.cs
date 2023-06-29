86e9dff1c0ceb0cfbed6b302cf984b95eb5b2c74
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-06-29 12:48:38 -0300

    chore: person and product repository

---
+++ MininuShop.Infra.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MininuShop.Domain;
+using MininuShop.Domain.Entities;
+using MininuShop.Infra.Data.Context;
+
+namespace MininuShop.Infra.Data;
+
+public class PersonRepository : IPersonRepository
+{
+  private readonly ApplicationDbContext _db;
+
+  public PersonRepository(ApplicationDbContext db)
+  {
+    _db = db;
+  }
+
+  public async Task<Person> CreateAsync(Person person)
+  {
+    _db.Add(person);
+    await _db.SaveChangesAsync();
+    return person;
+  }
+
+  public async Task EditAsync(Person person)
+  {
+    _db.Update(person);
+    await _db.SaveChangesAsync();
+
+  }
+
+  public async Task<Person> GetByIdAsync(int id)
+  {
+    return await _db.People.FirstOrDefaultAsync(x => x.Id == id);
+  }
+
+  public async Task<ICollection<Person>> GetPeopleAsync()
+  {
+    return await _db.People.ToListAsync();
+  }
+}
