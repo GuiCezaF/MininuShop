c0e1b04669044e0fa738b693dfa6825ed81958fd
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-06-29 12:48:30 -0300

    chore: person and product repository interface

---
+++ MininuShop.Domain/Repositories/Interfaces/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using MininuShop.Domain.Entities;
+
+namespace MininuShop.Domain;
+
+public interface IPersonRepository
+{
+  Task<Person> GetByIdAsync(int id);
+  Task<ICollection<Person>> GetPeopleAsync();
+  Task<Person> CreateAsync(Person person);
+  Task EditAsync(Person person);
+}
---
+++ MininuShop.Domain/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using MininuShop.Domain.Entities;
+
+namespace MininuShop.Domain;
+
+public interface IProductRepository
+{
+  Task<Product> GetByIdAsync(int id);
+  Task<ICollection<Product>> GetProductsAsync();
+  Task<Product> CreateAsync(Product product);
+  Task EditAsync(Product product);
+  Task DeleteAsync(Product product);
+}
