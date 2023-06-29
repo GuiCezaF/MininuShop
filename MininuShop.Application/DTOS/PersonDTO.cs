5e53a9f989b319e6827dff59b271e9081cb6a078
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-06-29 12:57:19 -0300

    chore: Add person DTO and DTO Validation

---
+++ MininuShop.Application/DTOS/PersonDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MininuShop.Application;
+
+public class PersonDTO
+{
+  public int Id { get; private set; }
+  public string Name { get; private set; }
+  public string Email { get; private set; }
+  public string Password { get; private set; }
+  public string Document { get; private set; }
+  public string Phone { get; private set; }
+}
---
+++ MininuShop.Application/DTOS/Validations/PersonDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace MininuShop.Application;
+
+public class PersonDTOValidator : AbstractValidator<PersonDTO>
+{
+  public PersonDTOValidator()
+  {
+
+    RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("O nome deve ser Informado");
+    RuleFor(x => x.Document).NotEmpty().NotNull().WithMessage("O Documento deve ser Informado");
+    RuleFor(x => x.Phone).NotEmpty().NotNull().WithMessage("O telefone deve ser Informado");
+    RuleFor(x => x.Password).NotEmpty().NotNull().WithMessage("A senha deve ser Informado");
+    RuleFor(x => x.Email).NotEmpty().NotNull().WithMessage("O Email deve ser Informado");
+
+  }
+}
