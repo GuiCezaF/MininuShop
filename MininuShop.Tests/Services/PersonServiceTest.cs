966a95523801f07cf6655e2c09edf88420c97a39
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-06-30 22:44:48 -0300

    test: create person

---
+++ MininuShop.Tests/Services/PersonServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MininuShop.Application;
+using MininuShop.Domain;
+using Moq;
+
+namespace MininuShop.Tests.Services
+{
+    public class PersonServiceTest
+    {
+        private PersonService _personService;
+        private IMapper _mapper;
+        private Mock<IPersonRepository> _personRepository;
+
+        public PersonServiceTest()
+        {
+            _personService = new PersonService(new Mock<IPersonRepository>().Object, new Mock<IMapper>().Object);
+        }
+
+        [Fact]
+        public void CreatePerson()
+        {
+            var person = new PersonDTO
+            {
+                Id = 1,
+                Name = "Gui",
+                Email = "teste@teste.com",
+                Password = "123456",
+                Document = "12345678789",
+                Phone = "12345676890"
+            };
+
+            var result = _personService.CreateAsync(person);
+            Assert.Equal(1, result.Id);
+        }
+    }
+}
